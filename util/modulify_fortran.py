3663f18f8db3d6d74b3747fecdf104f128ef4c27
mark <mark@rafiki>
2016-05-19 23:16:57 +0200

    applying modulify automatically

--- util/modulify_fortran.py
+++ util/modulify_fortran.py
@@ -6,18 +6,16 @@ This is a very crude script to find the information needed to
 turn a Fortran codebase into modules which match the files.
 """
 
-from re import sub as re_sub, findall
-from sys import argv, stderr
-from os.path import exists
+from collections import defaultdict
+from os import getcwd
+
+from re import findall
+from sys import argv, stdout, stderr
+from os.path import exists, basename, abspath
 
 """
 Get and check arguments.
 """
-# if len(argv) < 2:
-# 	stderr.write('no output target provided\n')
-# 	exit(4)
-# modname = argv[1]
-
 targets = argv[1:]
 quit = False
 
@@ -26,6 +24,8 @@ if not targets:
 	exit(2)
 
 for target in targets:
+	if target.startswith('-'):
+		continue
 	if not exists(target):
 		stderr.write('could not find "{0:s}"\n'.format(target))
 		quit = True
@@ -34,64 +34,86 @@ if quit:
 	exit(1)
 
 """
+Set some ignored names that can be called without import.
+"""
+ignore = {'mpi_finalize', 'mpi_comm_size', 'mpi_comm_rank', 'mpi_recv', 'mpi_send', 'mpi_allreduce', 'flush', 'flush_', 'getenv'}
+
+"""
 Read the source files.
 """
-# iface_words = {'function', 'end function', 'subroutine', 'end subroutine', 'recursive', 'pure', 'elemental',
-# 	'integer', 'real', 'double', 'complex', 'character', 'logical', 'parameter',}
-# output = ['      !! automatically generated interface', '      !! pipe a .f90 file & compile with -ffixed-form',
-# 	'', '      module {0:s}'.format(modname), '', '      interface', '']
-defined = {}
-called = {}
+defines = defaultdict(list)
+defined_in = {}
+called_in = defaultdict(set)
+calls = defaultdict(set)
 for target in targets:
-	# stderr.write('.')
-	stderr.write('{0:s}\n'.format(target))
+	if target.startswith('-'):
+		continue
 	with open(target, 'r') as fh:
 		content = fh.read().lower()
-	defined = findall(r'(?:function|subroutine)\s+(\w+)\s*\(', content)
-	print(defined)
-	called = findall(r'call\s+(\w+)\s*\(', content)
-	# functions are not included here
-	print(called)
+	for defi in findall(r'(?:function|subroutine)\s+(\w+)\s*\(', content):
+		if defi in defined_in:
+			stderr.write('{0:s} from {1:s} redefined in {2:s}\n'.format(defi, defined_in[defi], target))
+		defined_in[defi] = target
+		defines[target].append(defi)
+	for calli in findall(r'call\s+(\w+)\s*\(', content):
+		called_in[calli].add(target)
+		calls[target].add(calli)
+
+"""
+Construct use statements.
+"""
+use = defaultdict(lambda: defaultdict(set))
+for file, fcalls in calls.items():
+	frm = defaultdict(list)
+	for call in fcalls:
+		if call in ignore:
+			continue
+		if call in defined_in:
+			if file != defined_in[call]:
+				name = basename(defined_in[call].split('.')[0])
+				use[file][name].add(call)
+		else:
+			use[file][None].add(call)
+			# stderr.write('don\'t know where {0:s} is defined (called from {1:s})\n'.format(call, file))
+
+"""
+Construct modules (including use statements).
+"""
+for file, routines in defines.items():
+	stderr.write('{0:s}\n'.format(file))
+	stdout.write('      module {0:s}\n'.format(file.split('.')[0]))
+	for src, calls in use[file].items():
+		if src is not None:
+			stdout.write('      use {0:s}, only: {1:s}\n'.format(src, ', '.join(calls)))
+	if None in use[file]:
+		stdout.write('      !! unknown origin: {0:s}\n'.format(', '.join(use[file][None])))
+	stdout.write('      private\n')
+	public, internal = [], []
+	for routine in routines:
+		if {file} != called_in[routine]:
+			public.append(routine)
+		else:
+			internal.append(routine)
+	stdout.write('      public {0:s}\n'.format(', '.join(public)))
+	if internal:
+		stdout.write('      !! internal: {0:s}\n'.format(', '.join(internal)))
+	stdout.write('      contains\n')
+	stdout.write('      \n')
+	stdout.write('      end module {0:s}\n'.format(file.split('.')[0]))
 
 
-if False:
-	next_too = False
-	sub = []
-	for line in lines:
-		line = line.rstrip()
-		code = line.lstrip()
-		code = re_sub(r'"[^"]*"', '""', code)
-		code = re_sub(r'\'[^\']*\'', '\'\'', code)
-		if code.startswith('*') or code.startswith('c..'):
-			code = ''
-		code = code.split('!')[0]
-		# if code.endswith(','):
-		# 	line += '&'
-		if next_too or any(code.startswith(word) for word in iface_words):
-			if sub and 'end' not in code and ('subroutine' in code or 'function' in code):
-				stderr.write('skipping the rest of "{0:s}"\n'.format(target))
-				break
-			sub.append(line)
-			next_too = False
-			if code.endswith(',') or code.endswith('&'):
-				next_too = True
-	if len(sub) and not 'end subroutine' in sub[-1] and not 'end function' in sub[-1]:
-		strt = findall(r'(function|subroutine)\s+(\w+)\s*\(', sub[0])
-		if not strt:
-			stderr.write('could not determine appropriate ending for "{0:s}"'.format(sub[0]))
-			# continue
-		sub.append('      end {0:s} {1:s}'.format(strt[0][0], strt[0][1]))
-		# if 'function' in sub[0]:
-		# 	sub.append('      end function')
-		# elif 'subroutine' in sub[0]:
-		# 	sub.append('      end subroutine')
-		# else:
-		# 	stderr.write('invalid ending for "{0:s}" in "{1:s}"\n'.format(sub[0], target))
-		# 	continue
-	output.extend(sub)
-	output.append('')
-# output.extend(['      end interface', '', '      end module'])
-# print('\n'.join(output))
-# stderr.write('done\n')
+"""
+Apply the changes if requested. This will probably need manual fixing after.
+"""
+if '--apply' in argv[1:]:
+	for target in targets:
+		if target.startswith('-') or getcwd() not in abspath(target):
+			continue
+		with open(target, 'r') as fh:
+			content = fh.read()
+		if 'module' in content.lower():
+			stderr.write('{0:s} already has a module, not applying\n'.format(target))
+			continue
+
 
 
