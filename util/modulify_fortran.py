7df6e999eb0820d69092ecfcf1410ebb0633af63
mark <mark@rafiki>
2016-05-19 20:21:47 +0200

    modulify basics

---
+++ util/modulify_fortran.py
@@ -0,0 +1,97 @@
+
+#todo: everything
+
+"""
+This is a very crude script to find the information needed to
+turn a Fortran codebase into modules which match the files.
+"""
+
+from re import sub as re_sub, findall
+from sys import argv, stderr
+from os.path import exists
+
+"""
+Get and check arguments.
+"""
+# if len(argv) < 2:
+# 	stderr.write('no output target provided\n')
+# 	exit(4)
+# modname = argv[1]
+
+targets = argv[1:]
+quit = False
+
+if not targets:
+	stderr.write('no targets provided\n')
+	exit(2)
+
+for target in targets:
+	if not exists(target):
+		stderr.write('could not find "{0:s}"\n'.format(target))
+		quit = True
+
+if quit:
+	exit(1)
+
+"""
+Read the source files.
+"""
+# iface_words = {'function', 'end function', 'subroutine', 'end subroutine', 'recursive', 'pure', 'elemental',
+# 	'integer', 'real', 'double', 'complex', 'character', 'logical', 'parameter',}
+# output = ['      !! automatically generated interface', '      !! pipe a .f90 file & compile with -ffixed-form',
+# 	'', '      module {0:s}'.format(modname), '', '      interface', '']
+defined = {}
+called = {}
+for target in targets:
+	# stderr.write('.')
+	stderr.write('{0:s}\n'.format(target))
+	with open(target, 'r') as fh:
+		content = fh.read().lower()
+	defined = findall(r'(?:function|subroutine)\s+(\w+)\s*\(', content)
+	print(defined)
+	called = findall(r'call\s+(\w+)\s*\(', content)
+	# functions are not included here
+	print(called)
+
+
+if False:
+	next_too = False
+	sub = []
+	for line in lines:
+		line = line.rstrip()
+		code = line.lstrip()
+		code = re_sub(r'"[^"]*"', '""', code)
+		code = re_sub(r'\'[^\']*\'', '\'\'', code)
+		if code.startswith('*') or code.startswith('c..'):
+			code = ''
+		code = code.split('!')[0]
+		# if code.endswith(','):
+		# 	line += '&'
+		if next_too or any(code.startswith(word) for word in iface_words):
+			if sub and 'end' not in code and ('subroutine' in code or 'function' in code):
+				stderr.write('skipping the rest of "{0:s}"\n'.format(target))
+				break
+			sub.append(line)
+			next_too = False
+			if code.endswith(',') or code.endswith('&'):
+				next_too = True
+	if len(sub) and not 'end subroutine' in sub[-1] and not 'end function' in sub[-1]:
+		strt = findall(r'(function|subroutine)\s+(\w+)\s*\(', sub[0])
+		if not strt:
+			stderr.write('could not determine appropriate ending for "{0:s}"'.format(sub[0]))
+			# continue
+		sub.append('      end {0:s} {1:s}'.format(strt[0][0], strt[0][1]))
+		# if 'function' in sub[0]:
+		# 	sub.append('      end function')
+		# elif 'subroutine' in sub[0]:
+		# 	sub.append('      end subroutine')
+		# else:
+		# 	stderr.write('invalid ending for "{0:s}" in "{1:s}"\n'.format(sub[0], target))
+		# 	continue
+	output.extend(sub)
+	output.append('')
+# output.extend(['      end interface', '', '      end module'])
+# print('\n'.join(output))
+# stderr.write('done\n')
+
+
