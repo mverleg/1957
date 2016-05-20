e7f7443416af67f5608bc66aeeb013e10313d056
mark <mark@rafiki>
2016-05-20 10:42:34 +0200

    greatly extended modulify, it basically works but needs more testing

--- util/modulify_fortran.py
+++ util/modulify_fortran.py
@@ -8,15 +8,15 @@ turn a Fortran codebase into modules which match the files.
 
 from collections import defaultdict
 from os import getcwd
-
 from re import findall
 from sys import argv, stdout, stderr
 from os.path import exists, basename, abspath
 
+
 """
 Get and check arguments.
 """
-targets = argv[1:]
+targets = list(sorted(argv[1:]))
 quit = False
 
 if not targets:
@@ -41,10 +41,10 @@ ignore = {'mpi_finalize', 'mpi_comm_size', 'mpi_comm_rank', 'mpi_recv', 'mpi_sen
 """
 Read the source files.
 """
-defines = defaultdict(list)
+file_defines = defaultdict(list)
 defined_in = {}
 called_in = defaultdict(set)
-calls = defaultdict(set)
+file_calls = defaultdict(set)
 for target in targets:
 	if target.startswith('-'):
 		continue
@@ -54,24 +54,27 @@ for target in targets:
 		if defi in defined_in:
 			stderr.write('{0:s} from {1:s} redefined in {2:s}\n'.format(defi, defined_in[defi], target))
 		defined_in[defi] = target
-		defines[target].append(defi)
+		file_defines[target].append(defi)
 	for calli in findall(r'call\s+(\w+)\s*\(', content):
 		called_in[calli].add(target)
-		calls[target].add(calli)
+		file_calls[target].add(calli)
 
 """
 Construct use statements.
 """
+def modname(pth):
+	return basename(pth.split('.')[0]) + '_mod'
+
 use = defaultdict(lambda: defaultdict(set))
-for file, fcalls in calls.items():
+for file, fcalls in file_calls.items():
 	frm = defaultdict(list)
 	for call in fcalls:
 		if call in ignore:
 			continue
 		if call in defined_in:
 			if file != defined_in[call]:
-				name = basename(defined_in[call].split('.')[0])
-				use[file][name].add(call)
+				# name = basename(defined_in[call].split('.')[0]) + '_mod'
+				use[file][modname(defined_in[call])].add(call)
 		else:
 			use[file][None].add(call)
 			# stderr.write('don\'t know where {0:s} is defined (called from {1:s})\n'.format(call, file))
@@ -79,41 +82,46 @@ for file, fcalls in calls.items():
 """
 Construct modules (including use statements).
 """
-for file, routines in defines.items():
+for file, routines in file_defines.items():
 	stderr.write('{0:s}\n'.format(file))
-	stdout.write('      module {0:s}\n'.format(file.split('.')[0]))
+	pre = '      module {0:s}\n'.format(modname(file))
 	for src, calls in use[file].items():
 		if src is not None:
-			stdout.write('      use {0:s}, only: {1:s}\n'.format(src, ', '.join(calls)))
+			pre += '      use {0:s}, only: {1:s}\n'.format(src, ', '.join(calls))
 	if None in use[file]:
-		stdout.write('      !! unknown origin: {0:s}\n'.format(', '.join(use[file][None])))
-	stdout.write('      private\n')
+		pre += '      !! unknown origin: {0:s}\n'.format(', '.join(use[file][None]))
+	pre += '      private\n'
 	public, internal = [], []
 	for routine in routines:
 		if {file} != called_in[routine]:
 			public.append(routine)
 		else:
 			internal.append(routine)
-	stdout.write('      public {0:s}\n'.format(', '.join(public)))
+	pre += '      public {0:s}\n'.format(', '.join(public))
 	if internal:
-		stdout.write('      !! internal: {0:s}\n'.format(', '.join(internal)))
-	stdout.write('      contains\n')
-	stdout.write('      \n')
-	stdout.write('      end module {0:s}\n'.format(file.split('.')[0]))
-
-
-"""
-Apply the changes if requested. This will probably need manual fixing after.
-"""
-if '--apply' in argv[1:]:
-	for target in targets:
-		if target.startswith('-') or getcwd() not in abspath(target):
+		pre += '      !! internal: {0:s}\n'.format(', '.join(internal))
+	pre += '      contains\n'
+	post = '      end module {0:s}\n'.format(modname(file))
+	"""
+	Apply the changes if requested. This will probably need manual fixing after.
+	"""
+	if '--apply' in argv[1:]:
+		if file.startswith('-') or getcwd() not in abspath(file):
 			continue
-		with open(target, 'r') as fh:
+		with open(file, 'r') as fh:
 			content = fh.read()
 		if 'module' in content.lower():
-			stderr.write('{0:s} already has a module, not applying\n'.format(target))
+			stderr.write('{0:s} already has a module, not applying\n'.format(file))
 			continue
+		with open(file, 'w') as fh:
+			stderr.write('turning {0:s} into module'.format(file))
+			fh.write('\n' + pre + '\n')
+			fh.write(content + '\n')
+			fh.write(post + '\n')
+	else:
+		stdout.write(pre)
+		stdout.write('\n')
+		stdout.write(post)
 
 
 
